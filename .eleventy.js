358ad3fe018d278d92f89286145b92a57ad862c0
Kevin Zhong <36215258+CLCK0622@users.noreply.github.com>
2022-12-28 09:11:17 +0800

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,67 @@
+// 11ty Plugins
+const socialImages = require("@11tyrocks/eleventy-plugin-social-images");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
+// Helper packages
+const slugify = require("slugify");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+
+// Local utilities/data
+const packageVersion = require("./package.json").version;
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(socialImages);
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(pluginRss);
+
+  eleventyConfig.addWatchTarget("./src/sass/");
+
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/fonts");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/favicon.png");
+
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  eleventyConfig.addShortcode("packageVersion", () => `v${packageVersion}`);
+
+  eleventyConfig.addFilter("slug", (str) => {
+    if (!str) {
+      return;
+    }
+
+    return slugify(str, {
+      lower: true,
+      strict: true,
+      remove: /["]/g,
+    });
+  });
+
+  /* Markdown Overrides */
+  let markdownLibrary = markdownIt({
+    html: true,
+  }).use(markdownItAnchor, {
+    permalink: markdownItAnchor.permalink.ariaHidden({
+      class: "tdbc-anchor",
+      space: false,
+    }),
+    level: [1, 2, 3],
+    slugify: (str) =>
+      slugify(str, {
+        lower: true,
+        strict: true,
+        remove: /["]/g,
+      }),
+  });
+  eleventyConfig.setLibrary("md", markdownLibrary);
+
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      output: "public",
+      layouts: "_layouts",
+    },
+  };
+};
